e6cbf2280a37b7e46806e01cc41fd57a16533d08
Matthias Rampke <matthias@rampke.de>
2015-03-08 11:50:05 +0100

    less output during import

--- bin/import.rb
+++ bin/import.rb
@@ -19,19 +19,17 @@ channel_dirs.each do |dir|
   channel_name = File.basename(dir)
   Channel.find_or_create(:name => channel_name)
 
-  log_files = Dir.glob(File.join(dir, '????-??-??.log'))
+  log_files = Dir.glob(File.join(dir, '????-??-??.log')).sort
   log_files.each_slice(log_files.length/8) do |slice|
-    print '.'
     Thread.new do
       slice.each do |file|
         DB.transaction do
           day = File.basename(file, '.log')
+          puts "#{channel_name} #{day}"
           File.open(file).each do |line|
             m = Logformat::Message.parse_irssi_line(day, channel_name, line)
-            print m.type[0] unless m.nil?
           end
         end
-        puts ''
       end
     end
   end
