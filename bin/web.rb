2c06db8750b5a1407e331a326b563e3056bf08c4
Matthias Rampke <matthias@rampke.de>
2015-04-19 08:40:47 +0200

    break out 404/401 returns into helper
    for DRY.

--- bin/web.rb
+++ bin/web.rb
@@ -15,6 +15,24 @@ helpers do
   def h(text)
     Rack::Utils.escape_html(text)
   end
+
+  def check_channel_access!
+    user = User.from_env(request.env)
+    channel = Channel.find(:slug => params[:channel])
+    if channel.nil?
+      halt 404, erb(:error, :locals => {
+        :title => 'Logs: Not found',
+        :text => 'No such channel',
+      })
+    else
+      if channel.allowed?(user)
+        channel
+      else
+        headers['WWW-Authenticate'] = 'Basic realm="Logformat"'
+        halt 401
+      end
+    end
+  end
 end
 
 
@@ -30,6 +48,8 @@ get '/-/whoami' do
 end
 
 get '/:channel/:date' do
+  channel = check_channel_access!
+
   begin
     date = DateTime.strptime(params[:date], '%Y-%m-%d')
   rescue ArgumentError => e
@@ -40,62 +60,33 @@ get '/:channel/:date' do
     }
   end
 
-  user = User.from_env(request.env)
-  channel = Channel.find(:slug => params[:channel])
-  if channel.nil?
-    status 404
-    erb :error, :locals => {
-      :title => 'Logs: Not found',
-      :text => 'No such channel',
-    }
-  else
-    if channel.allowed?(user)
-      erb :channel, :locals => {
-        :title => "Logs for #{channel.name}, #{date.strftime('%Y-%m-%d')}",
-        :messages => Message
-          .filter(:channel => channel)
-          .where(['time BETWEEN ? AND ?', date, date+1])
-          .order(:time, :id),
-        :date => date,
-        :channel => channel,
-      }
-    else
-      status 401
-      headers['WWW-Authenticate'] = 'Basic realm="Logformat"'
-    end
-  end
+  erb :channel, :locals => {
+    :title => "Logs for #{channel.name}, #{date.strftime('%Y-%m-%d')}",
+    :messages => Message
+      .filter(:channel => channel)
+      .where(['time BETWEEN ? AND ?', date, date+1])
+      .order(:time, :id),
+    :date => date,
+    :channel => channel,
+  }
 end
 
 # NOTE: this is quite expensive (sequential scan on messages)
 get '/:channel' do
-  user = User.from_env(request.env)
-  channel = Channel.find(:slug => params[:channel])
-  if channel.nil?
-    status 404
-    erb :error, :locals => {
-      :title => 'Logs: Not found',
-      :text => 'No such channel',
-    }
-  else
-    if channel.allowed?(user)
-      erb :channel_days, :locals => {
-        :title => "Logs for #{channel.name}",
-        :channel => channel,
-        :days => Message
-          .filter(:channel => channel)
-          .select(
-            Sequel.as(
-              Sequel.function(:date, :time),
-              :date
-            )
-          )
-          .group(:date)
-          .order(Sequel.desc(:date))
-          .map(:date)
-      }
-    else
-      status 401
-      headers['WWW-Authenticate'] = 'Basic realm="Logformat"'
-    end
-  end
+  channel = check_channel_access!
+  erb :channel_days, :locals => {
+    :title => "Logs for #{channel.name}",
+    :channel => channel,
+    :days => Message
+      .filter(:channel => channel)
+      .select(
+        Sequel.as(
+          Sequel.function(:date, :time),
+          :date
+        )
+      )
+      .group(:date)
+      .order(Sequel.desc(:date))
+      .map(:date)
+  }
 end
